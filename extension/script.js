5ee90f2463662ae6cf654b476f3b748941711cf4
Faraan_R <48865049+WWFYOcelot@users.noreply.github.com>
2024-02-17 12:21:10 -0500

    Delete extension directory

--- extension/script.js
+++
@@ -1,7 +0,0 @@
-async function fetchData() {
-    const res=await fetch ("api_address");
-    const record=await res.json();
-    document.getElementById("date").innerHTML=record.data[0].date;
-    
-}
-fetchData();
